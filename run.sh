3b0c453c5fd0de812b0193e587b1240a3b73a518
luochuang <rekkles_hb@163.com>
2023-08-16 15:05:51 +0800

    添加了一个脚本

---
+++ run.sh
@@ -0,0 +1,12 @@
+# run.sh
+# 切换到jar包目录下
+cd /home/ubuntu/GitHub_Action
+# 杀死之前的项目进程。这里的cut -c9-14是截取进程id，不一定都是-c9-14
+# 可以先运行ps -ef|grep java|grep GitHub_Action-0.0.1-SNAPSHOT.jar看一下进程id是第几位到第几位
+ps -ef|grep java|grep software-0.0.1-SNAPSHOT.jar|cut -c9-14|xargs kill -9
+# 停5秒
+sleep 5s
+# 使环境变量生效
+source /root/.bash_profile
+# 运行项目
+nohup java -jar software-0.0.1-SNAPSHOT.jar >/home/ubuntu/cicd.log 2>&1 &
